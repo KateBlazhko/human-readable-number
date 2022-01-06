e845f9d5c729b0a421d9b1c0b7606f8966e7ad27
KateBlazhko <kateugnicheva@gmail.com>
2022-01-06 13:45:14 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+let firstDecade = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let secondDecade = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let allDecade = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
 module.exports = function toReadable (number) {
-  
+  return (number < 10) ? firstDecade[number] :
+         (number < 100) ? checkDoubleFigure(number) : checkThreeFigure(number);
+}
+
+const checkDoubleFigure = function (number) {
+  let numberDecada = Math.trunc(number / 10);
+  let numberLast = number % 10;
+
+  return (numberDecada === 1) ? secondDecade[numberLast] :
+         (numberLast === 0) ? allDecade[numberDecada] :
+         allDecade[numberDecada] + ' ' + firstDecade[numberLast];
+
+}
+
+const checkThreeFigure = function (number) {
+  let numberHundred = Math.trunc(number / 100);
+  let numberDecada = (Math.trunc(number / 10)) % 10;
+  let numberLast = number % 10;
+
+  return ((numberLast === 0) && (numberDecada === 0)) ? (firstDecade[numberHundred]  + ' hundred') :
+         (numberDecada === 0) ? (firstDecade[numberHundred] + ' hundred ' + firstDecade[numberLast]) :
+         (numberDecada === 1) ? (firstDecade[numberHundred] + ' hundred ' + secondDecade[numberLast]) :
+         (numberLast === 0) ? (firstDecade[numberHundred] + ' hundred ' + allDecade[numberDecada]) :
+         (firstDecade[numberHundred] + ' hundred ' + allDecade[numberDecada] + ' ' + firstDecade[numberLast])
 }
